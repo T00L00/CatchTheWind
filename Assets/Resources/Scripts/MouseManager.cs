91bded6dcc94479ab3c01e6c5ff3db5d7849e27a
ChickeNuggetGuy <hawkinsmalikk11@gmail.com>
2021-04-14 00:58:29 -0500

    Test?

---
+++ Assets/Resources/Scripts/MouseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseManager : MonoBehaviour
+{
+    public static MouseManager instance;
+    public GameObject mouseFollower;
+
+    public Vector3 mousePos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Mouse.current.position.ReadValue();
+        mousePos.z = Camera.main.farClipPlane;
+        mouseFollower.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(mousePos, 2.5f);
+    }
+}
---
+++ Assets/Resources/Scripts/WInd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class WInd : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Test " + other.name);
+        other.transform.position = Vector3.zero;
+    }
+}
